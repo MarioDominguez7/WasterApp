523061e35017ecadda492a4076390ad1dedecbc7
Mario <mariodominguez1809@gmail.com>
2024-10-22 20:17:00 -0600

    Actualizaciones

--- app/src/main/java/mx/tecnm/cdhidalgo/wasterapp/LogOut.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/wasterapp/LogOut.kt
@@ -26,8 +26,9 @@ class LogOut : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        /* binding = ActivityLogOutBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_log_out)
+        /* enableEdgeToEdge()
+        binding = ActivityLogOutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Cargar el GIF usando Glide
@@ -61,10 +62,10 @@ class LogOut : AppCompatActivity() {
             finish()
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
